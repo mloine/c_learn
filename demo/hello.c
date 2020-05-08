9532789864297f0ee0290f21e1577d19baec45dc
xueyongkang <m15618975413@163.com>
2020-05-08 12:03:07 +0800

    基础语法

--- demo/hello.c
+++ demo/hello.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main()
-{
+{	
+	//输出 字符
 	printf("hello world xueyongkang \n");
 	return 0;
 }
---
+++ demo/sizeof.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <limits.h>
+int main()
+{	
+	//查看类型在平台上的准确大小
+	printf("storage size for int :%d \n",sizeof(int));
+	return 0;
+}
